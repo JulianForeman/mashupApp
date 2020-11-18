12c04814ba382148cfb2bc68728ebd7b10bda4cc
JulianForeman <julianforeman@me.com>
2020-11-18 10:26:41 -0800

    Canvas Functionality

--- mashup app/public/js/canvas.js
+++ mashup app/public/js/canvas.js
@@ -5,9 +5,18 @@
   var previous_y;
   var clear_button = document.getElementById('clear');
   var canvas_area = document.getElementById('canvas_area');
+  var pencil = document.getElementById('pencil');
+  var paintbrush = document.getElementById('brush');
+  var eraser = document.getElementById('eraser');
+  var sizeSlider = document.getElementById('size');
+  var colourPicker = document.getElementById('color');
 
   clear_button.addEventListener('click', () => ctx.clearRect(0,0,canvas.width, canvas.height));
 
+  var drawWidth = 5;
+  var drawColour = '#000000';
+  var drawStyle = 'round';
+  sizeSlider.value = 0;
 
   var down = event => {
     previous_x = event.offsetX;
@@ -28,9 +37,9 @@
     var y = event.offsetY;
 
     ctx.beginPath();
-    ctx.strokeStyle = '#00f';
-    ctx.lineJoin = 'round';
-    ctx.lineWidth = 5;
+    ctx.strokeStyle = drawColour;
+    ctx.lineJoin = drawStyle;
+    ctx.lineWidth = drawWidth;
     ctx.moveTo(previous_x,previous_y);
     ctx.lineTo(x,y);
     ctx.closePath();
@@ -38,6 +47,35 @@
     previous_x = x;
     previous_y = y;
   };
+
+  pencil.addEventListener('click', () => {
+    var using_pencil = true;
+    drawWidth = 2;
+    drawStyle = 'square';
+  });
+
+  paintbrush.addEventListener('click', () => {
+    using_pencil = false;
+    drawWidth = sizeSlider.value;
+    drawColour = colourPicker.value;
+    drawStyle = 'round';
+  });
+
+  eraser.addEventListener('click', () => {
+    drawWidth = sizeSlider.value;
+    drawColour = '#ffffff';
+  });
+
+  sizeSlider.addEventListener('change', () => {
+    if (using_pencil === false) {
+      drawWidth = sizeSlider.value;
+    }
+  });
+
+  colourPicker.addEventListener('change', () => {
+    drawColour = colourPicker.value;
+  });
+
 
   canvas.addEventListener('mousedown', down);
   canvas.addEventListener('touchstart', down);
